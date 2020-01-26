134768b7c035e0aa268ac58ae78269a033305cbf
unknown <gracejyzhang00@gmail.com>
2020-01-25 22:02:12 -0500

    Speech to text and sentiment analysis

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static readonly string key = Values.getKey();
+        private static readonly string endpoint = Values.getEndpoint();
+
+        static void Main(string[] args)
+        {
+            string filename = @"C:\Users\grace\source\repos\ConsoleApp1\ConsoleApp1\whatstheweatherlike.wav";
+
+            if (args.Length > 0)
+            {
+                filename = args[0];
+            }
+
+            var db_client = new MongoClient(Values.GetDatabase());
+            var db = db_client.GetDatabase("test");
+            var collection = db.GetCollection<BsonDocument>("audio");
+
+            AudioToText audio = new AudioToText();
+            audio.RecognizeSpeech(filename).Wait();
+            List<string> sentences = audio.sentences;
+
+            SentimentAnalysis analyzer = new SentimentAnalysis();
+            foreach (string sentence in sentences)
+            {
+                analyzer.Analyze(endpoint, key, sentence).Wait();
+            }
+
+            KeyPhraseExtraction phrases = new KeyPhraseExtraction();
+            phrases.Extract(endpoint, key, sentences).Wait();
+
+            var document = new BsonDocument { { "date", 10000 }, { "sentences", analyzer.array }, { "key_phrases", phrases.array } };
+
+            //SentimentAnalysisExample(client);
+            //KeyPhraseExtractionExample(client);
+
+            collection.InsertOne(document);
+
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/SentimentAnalysis.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class SentimentAnalysis
+    {
+        public BsonArray array;
+        private int id = 1;
+
+        public SentimentAnalysis() {
+            array = new BsonArray();
+        }
+
+        public async Task Analyze(string endpoint, string key, string sentence)
+        {
+            var credentials = new ApiKeyServiceClientCredentials(key);
+            var client = new TextAnalyticsClient(credentials)
+            {
+                Endpoint = endpoint
+            };
+
+            var result = client.Sentiment(sentence, "en");
+
+            array.Add(new BsonDocument { { "id", id }, { "sentence", sentence }, { "score", result.Score} });
+            id++;
+            Console.WriteLine($"Sentiment Score: {result.Score:0.00}");
+
+            //// The documents to be analyzed. Add the language of the document. The ID can be any value.
+            //var inputDocuments = new MultiLanguageBatchInput(
+            //    new List<MultiLanguageInput>
+            //    {
+            //        new MultiLanguageInput("1", "I had the best day of my life.", "en"),
+            //        new MultiLanguageInput("2", "This was a waste of my time. The speaker put me to sleep.", "en"),
+            //        new MultiLanguageInput("3", "No tengo dinero ni nada que dar...", "es"),
+            //        new MultiLanguageInput("4", "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura.", "it"),
+            //    });
+
+            //var result = await client.SentimentBatchAsync(inputDocuments);
+
+            //// Printing sentiment results
+            //Console.WriteLine("===== Sentiment Analysis =====\n");
+
+            //foreach (var document in result.Documents)
+            //{
+            //    Console.WriteLine($"Document ID: {document.Id} , Sentiment Score: {document.Score:0.00}");
+            //}
+            //Console.WriteLine();
+        }
+    }
+}
